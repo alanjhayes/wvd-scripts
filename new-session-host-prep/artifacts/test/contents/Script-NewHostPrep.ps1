5ba9b18e5bf1dc1f870846773571b93f729b170d
Alan H <51532898+alanjhayes@users.noreply.github.com>
2020-11-05 16:25:12 +0000

    Update Script-NewHostPrep.ps1

--- new-session-host-prep/artifacts/test/contents/Script-NewHostPrep.ps1
+++ new-session-host-prep/artifacts/test/contents/Script-NewHostPrep.ps1
@@ -1,8 +1,11 @@
 
-$ScriptPath = [system.io.path]::GetDirectoryName($PSCommandPath)
+$ScriptPath   = [system.io.path]::GetDirectoryName($PSCommandPath)
+$testfile     = "c:\testfile.txt"
 
 Write-host "$scriptpath"
+Write-host "Writing dummy file to the $testfile"
 
-Write-host "Writing dummy file to the c:\testfile.txt"
-"hello world " | out-file "c:\testfile.txt"
+"hello world " | out-file $testfile
+"$scriptpath"  | out-file $testfile -append
+"$(get-date)"  | out-file $testfile -append
 
