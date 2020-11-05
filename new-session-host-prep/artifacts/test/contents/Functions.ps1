9a0fb4cfd1ea48352e1449d5c5d6b501319348c9
Alan H <51532898+alanjhayes@users.noreply.github.com>
2020-11-05 14:26:42 +0000

    Update Functions.ps1

--- new-session-host-prep/artifacts/test/contents/Functions.ps1
+++ new-session-host-prep/artifacts/test/contents/Functions.ps1
@@ -1,1 +1,56 @@
+<#
+
+.SYNOPSIS
+Functions/Common variables file to be used by scripts
+
+#>
+
+# Variables
+
+# Setting to Tls12 due to Azure web app security requirements
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+function Write-Log
+{ 
+    [CmdletBinding()] 
+    param
+    ( 
+        [Parameter(Mandatory = $false)] 
+        [string]$Message,
+        [Parameter(Mandatory = $false)] 
+        [string]$Error 
+    ) 
+     
+    try
+    { 
+        $DateTime = Get-Date -Format "MM-dd-yy HH:mm:ss"
+        $Invocation = "$($MyInvocation.MyCommand.Source):$($MyInvocation.ScriptLineNumber)" 
+        if ($Message)
+        {
+            Add-Content -Value "$DateTime - $Invocation - $Message" -Path "$([environment]::GetEnvironmentVariable('TEMP', 'Machine'))\ScriptLog.log" 
+        }
+        else
+        {
+            Add-Content -Value "$DateTime - $Invocation - $Error" -Path "$([environment]::GetEnvironmentVariable('TEMP', 'Machine'))\ScriptLog.log" 
+        }
+    } 
+    catch
+    { 
+        Write-Error $_.Exception.Message 
+    } 
+}
+
+function Is1809OrLater
+{
+    $OSVersionInfo = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+    if ($OSVersionInfo -ne $null)
+    {
+        if ($OSVersionInfo.ReleaseId -ne $null)
+        {
+            Write-Log -Message "Build: $($OSVersionInfo.ReleaseId)"
+            $rdshIs1809OrLaterBool=@{$true = $true; $false = $false}[$OSVersionInfo.ReleaseId -ge 1809]
+        }
+    }
+    return $rdshIs1809OrLaterBool
+}
 
